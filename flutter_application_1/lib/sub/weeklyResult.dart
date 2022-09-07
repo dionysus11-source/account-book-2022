19b1623f55bc2eb56c61077b37e0c110df577fa8
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2022-09-07 23:05:48 +0900

    Add pie chart

--- flutter_application_1/lib/sub/weeklyResult.dart
+++ flutter_application_1/lib/sub/weeklyResult.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../object/AccountApplicationService.dart';
 import '../object/account.dart';
-import 'package:intl/intl.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WeeklyResult extends StatefulWidget {
   final Future<List> db;
@@ -14,53 +14,63 @@ class WeeklyResult extends StatefulWidget {
 }
 
 class _WeeklyResultState extends State<WeeklyResult> {
+  late Map<String, double> dataMap = {'식비': 0};
+  DateTime selectedDate = DateTime.now();
+
+  int getWeek(DateTime date) {
+    int day = date.day - 1;
+    int ret = (day ~/ 7).toInt() + 1;
+    return ret;
+  }
+
+  void calcMap() {
+    int week = getWeek(selectedDate);
+    widget.db.then((value) {
+      value.forEach((element) {
+        int elementWeek = getWeek(DateTime.parse(element.date));
+        if (elementWeek == week) {
+          dataMap.update(element.category, (value) => value + element.ammount,
+              ifAbsent: () => element.ammount.toDouble());
+        }
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    calcMap();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          child: Center(
-        child: FutureBuilder(
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<dynamic> data = snapshot.data as List<dynamic>;
-
-              data.sort((a, b) {
-                return a.date
-                    .toString()
-                    .toLowerCase()
-                    .compareTo(b.date.toString().toLowerCase());
-              });
-              data = data.reversed.toList();
-              int length = data.length;
-              var f = NumberFormat('###,###,###,###');
-              return ListView.builder(
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 5,
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 20),
-                    child: ListTile(
-                      title: Text(data[index].content),
-                      subtitle: Text(data[index].category),
-                      leading: Text(data[index].date),
-                      trailing:
-                          Text(f.format(data[index].ammount).toString() + '원'),
-                    ),
-                  );
-                },
-                itemCount: length,
-              );
-            }
-            return const Text('No data');
-          },
-          future: widget.db,
+      body: PieChart(
+        dataMap: dataMap,
+        animationDuration: const Duration(milliseconds: 800),
+        chartLegendSpacing: 32,
+        chartRadius: MediaQuery.of(context).size.width / 3.2,
+        initialAngleInDegree: 0,
+        chartType: ChartType.ring,
+        ringStrokeWidth: 32,
+        legendOptions: const LegendOptions(
+          showLegendsInRow: false,
+          legendPosition: LegendPosition.bottom,
+          showLegends: true,
+          legendTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      )),
+        chartValuesOptions: const ChartValuesOptions(
+          showChartValueBackground: true,
+          showChartValues: true,
+          showChartValuesInPercentage: false,
+          showChartValuesOutside: false,
+          decimalPlaces: 1,
+        ),
+        // gradientList: ---To add gradient colors---
+        // emptyColorGradient: ---Empty Color gradient---
+      ),
     );
   }
 }
