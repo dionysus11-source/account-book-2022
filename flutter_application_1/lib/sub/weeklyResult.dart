8eab445a1bc5f98aac4787f8b3ae16ef9f74d1b8
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2022-09-08 13:17:57 +0900

    Add weekly datamap

--- flutter_application_1/lib/sub/weeklyResult.dart
+++ flutter_application_1/lib/sub/weeklyResult.dart
@@ -15,7 +15,8 @@ class WeeklyResult extends StatefulWidget {
 }
 
 class _WeeklyResultState extends State<WeeklyResult> {
-  late Map<String, double> dataMap = {'식비': 0};
+  late Map<String, double> weeklyDataMap = {'식비': 0};
+  late Map<String, double> monthlyDataMap = {'식비': 0};
   DateTime selectedDate = DateTime.now();
 
   int getWeek(DateTime date) {
@@ -52,9 +53,10 @@ class _WeeklyResultState extends State<WeeklyResult> {
     return temp;
   }
 
-  updateMap(List data) {
+  updateMaps(List data) {
     setState(() {
-      dataMap = calcWeekMap(data);
+      weeklyDataMap = calcWeekMap(data);
+      monthlyDataMap = calcMonthMap(data);
     });
   }
 
@@ -62,76 +64,83 @@ class _WeeklyResultState extends State<WeeklyResult> {
   void initState() {
     super.initState();
     widget.db.then((value) {
-      updateMap(value);
+      updateMaps(value);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text(
-          '가계부',
-          style: TextStyle(color: Colors.black),
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              final selected = showDatePicker(
-                  context: context,
-                  initialDate: selectedDate,
-                  firstDate: DateTime(2022),
-                  lastDate: DateTime(2030));
-              late Future<List> newData;
-              selected.then((dateTime) {
-                selectedDate = dateTime as DateTime;
-                final String dateStr =
-                    DateFormat('yyyyMM').format(selectedDate);
-                newData =
-                    widget.applicationservice.then((val) => val.load(dateStr));
-                newData.then((value) {
-                  updateMap(value);
-                });
-                // 가계부 리스트  업데이트 추가
-              });
-            },
-            child: Text(
-              DateFormat('yyyy-MM-dd').format(selectedDate),
-              style: const TextStyle(color: Colors.black),
-            ),
-          )
-        ],
-      ),
-      body: PieChart(
-        dataMap: dataMap,
-        animationDuration: const Duration(milliseconds: 800),
-        chartLegendSpacing: 32,
-        chartRadius: MediaQuery.of(context).size.width / 2.5,
-        initialAngleInDegree: 0,
-        chartType: ChartType.ring,
-        ringStrokeWidth: 32,
-        legendOptions: const LegendOptions(
-          showLegendsInRow: false,
-          legendPosition: LegendPosition.bottom,
-          showLegends: true,
-          legendTextStyle: TextStyle(
-            fontWeight: FontWeight.normal,
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: const Text(
+            '가계부',
+            style: TextStyle(color: Colors.black),
           ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                final selected = showDatePicker(
+                    context: context,
+                    initialDate: selectedDate,
+                    firstDate: DateTime(2022),
+                    lastDate: DateTime(2030));
+                late Future<List> newData;
+                selected.then((dateTime) {
+                  selectedDate = dateTime as DateTime;
+                  final String dateStr =
+                      DateFormat('yyyyMM').format(selectedDate);
+                  newData = widget.applicationservice
+                      .then((val) => val.load(dateStr));
+                  newData.then((value) {
+                    updateMaps(value);
+                  });
+                });
+              },
+              child: Text(
+                DateFormat('yyyy-MM-dd').format(selectedDate),
+                style: const TextStyle(color: Colors.black),
+              ),
+            )
+          ],
         ),
-        chartValuesOptions: const ChartValuesOptions(
-          showChartValueBackground: true,
-          showChartValues: true,
-          showChartValuesInPercentage: false,
-          showChartValuesOutside: false,
-          decimalPlaces: 1,
-        ),
-        // gradientList: ---To add gradient colors---
-        // emptyColorGradient: ---Empty Color gradient---
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 50,
+            ),
+            PieChart(
+              dataMap: weeklyDataMap,
+              animationDuration: const Duration(milliseconds: 800),
+              chartLegendSpacing: 32,
+              chartRadius: MediaQuery.of(context).size.width / 2.5,
+              initialAngleInDegree: 0,
+              chartType: ChartType.ring,
+              ringStrokeWidth: 32,
+              legendOptions: const LegendOptions(
+                showLegendsInRow: false,
+                legendPosition: LegendPosition.bottom,
+                showLegends: true,
+                legendTextStyle: TextStyle(
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              chartValuesOptions: const ChartValuesOptions(
+                showChartValueBackground: true,
+                showChartValues: true,
+                showChartValuesInPercentage: false,
+                showChartValuesOutside: false,
+                decimalPlaces: 1,
+              ),
+              // gradientList: ---To add gradient colors---
+              // emptyColorGradient: ---Empty Color gradient---
+            ),
+            const SizedBox(height: 30),
+            Text(selectedDate.month.toString() + '월 종합 지출 내역'),
+          ],
+        ));
   }
 }
